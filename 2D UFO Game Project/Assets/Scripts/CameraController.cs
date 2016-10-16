0b903157b5897e838b5b491e7663104501848c63
Monkey 'sWorld <Monkey 'sWorld>
2016-10-16 12:42:11 +0600

    ok

---
+++ 2D UFO Game Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject Player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - Player.transform.position;
+	}
+
+	// LateUpdate is called once aftar all update for per frame
+	void LateUpdate () {
+		transform.position = Player.transform.position + offset;
+	}
+}
---
+++ 2D UFO Game Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody2D rb2d;
+	private int count;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		ShowCount ();
+		winText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) // When a obeject touch another obeject then call this function and make the veriable TRUE 
+	{
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			ShowCount ();
+		}
+	}
+
+	void ShowCount()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count > 14)
+		{
+			winText.text = "Hay Human you are WIN !!!";
+		}
+	}
+
+	void update()
+	{
+		
+	}
+}
